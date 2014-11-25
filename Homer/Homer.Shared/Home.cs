f40b9ca30bfb0253571c75b0a79f74eb2dff538b
dharper70 <dale.harper@outlook.com>
2014-11-24 18:06:31 -0800

    First Commit

---
+++ Homer/Homer.Shared/Home.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homer
+{
+    public class Home
+    {
+        public String StreetNumber { get; set; }
+        public String StreetName { get; set; }
+        public String City { get; set; }
+        public String PostalCode { get; set; }
+        public String Province { get; set; }
+        public String PolicyNumber { get; set; }
+
+    }
+}
---
+++ Homer/Homer.Windows/AddPhotos.xaml.cs
@@ -0,0 +1,161 @@
+﻿using Homer.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Media;
+using Windows.Media.Capture;
+using Windows.Media.MediaProperties;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234237
+
+namespace Homer
+{        
+    /// <summary>
+    /// A basic page that provides characteristics common to most applications.
+    /// </summary>
+    public sealed partial class AddPhotos : Page
+    {
+        Home clientHome = new Home();
+        BitmapImage currentBMP = new BitmapImage();
+
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        /// <summary>
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// NavigationHelper is used on each page to aid in navigation and 
+        /// process lifetime management
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        
+        public AddPhotos()
+        {
+            this.InitializeComponent();
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += navigationHelper_LoadState;
+            this.navigationHelper.SaveState += navigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation. Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session. The state will be null the first time a page is visited.</param>
+        private void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void navigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        async private void takephotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            CameraCaptureUI cameraUI = new CameraCaptureUI();
+
+            cameraUI.PhotoSettings.CroppedAspectRatio = new Size(4, 3);
+
+            Windows.Storage.StorageFile capturedMedia =
+                await cameraUI.CaptureFileAsync(CameraCaptureUIMode.Photo);
+
+            if (capturedMedia != null)
+            {
+                BitmapImage bmp = new BitmapImage();
+                IRandomAccessStream stream = await capturedMedia.OpenAsync(FileAccessMode.Read);
+                bmp.SetSource(stream);
+                imagePreview.Source = bmp;
+                currentBMP = bmp;                                
+            }
+
+        }
+
+        private void addPhotoButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddPhoto();
+        }
+
+        private void AddPhoto()
+        {
+            ListBoxItem lbi = catagoryListBox.SelectedItem as ListBoxItem;
+            catagoryText.Text = lbi.Content.ToString();
+            Image img = new Image();
+            img.MaxHeight = 150;
+            img.MaxWidth = 200;
+            img.Margin = new Thickness(0, 0, 10, 0);
+            img.Source = currentBMP;            
+            filmStrip.Children.Add(img);
+                        
+            BitmapImage bi3 = new BitmapImage();
+            bi3.DecodePixelWidth = 400;
+            bi3.DecodePixelHeight = 300;
+            bi3.UriSource = new Uri(img.BaseUri, "Assets/ClickHere.png");
+
+            imagePreview.Source = bi3;
+
+        }            
+               
+        
+                
+        #region NavigationHelper registration
+
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// 
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="GridCS.Common.NavigationHelper.LoadState"/>
+        /// and <see cref="GridCS.Common.NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+    }
+}
